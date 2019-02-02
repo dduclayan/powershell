ec69fb1b17629c6139116be049643ad935165503
dduclayan <dduclayan@gmail.com>
2019-02-02 09:45:30 -1000

    Add files via upload

---
+++ lastlogon.ps1
@@ -0,0 +1,7 @@
+﻿<#
+SYNOPSIS
+.This code will pull all ad users and their lastlogontimestamp
+#>
+
+
+Invoke-Command -ComputerName AD01 {Get-aduser -filter * -properties lastlogon | select samaccountname, @{n='lastlogon';e={[datetime]::FromFileTime($_.lastlogon)}}} | select -Property * -ExcludeProperty PSComputerName, RunspaceId | export-csv C:\Windowspowershell\reports\lastLogOnReport.csv
