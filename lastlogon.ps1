4a24bee2bc2786f1ae94a766eb1c4cae56ff105c
Devin Duclayan <dduclayan@gmail.com>
2019-02-13 12:55:21 -1000

    Update lastlogon.ps1

--- lastlogon.ps1
+++ lastlogon.ps1
@@ -1,7 +1,8 @@
-﻿<#
+<#
 SYNOPSIS
 .This code will pull all ad users and their lastlogontimestamp
 #>
 
+$filename = "ADLastLogOn" + (get-date -format "-MM-dd-yyyy-hhmmtt")
 
-Invoke-Command -ComputerName AD01 {Get-aduser -filter * -properties lastlogon | select samaccountname, @{n='lastlogon';e={[datetime]::FromFileTime($_.lastlogon)}}} | select -Property * -ExcludeProperty PSComputerName, RunspaceId | export-csv C:\Windowspowershell\reports\lastLogOnReport.csv
+Invoke-Command -ComputerName AD01 {Get-aduser -filter * -properties lastlogon | select samaccountname, @{n='lastlogon';e={[datetime]::FromFileTime($_.lastlogon)}}} | select -Property * -ExcludeProperty PSComputerName, RunspaceId | export-csv C:\Users\$env:USERNAME\downloads\$filename.csv
